d6c8e1d0af2d04f07c5a8f83c92206dc8ebaceaa
DESTINY <DESTINY@DESKTOP-7HI9NSL>
2022-01-30 15:13:09 +0100

    Ejercicio Github¡

---
+++ MeloMiguel/src/melomiguel/Melo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package melomiguel;
+
+/**
+ *
+ * @author DESTINY
+ */
+public class Melo {
+      public static void main(String[] args) {
+          System.out.println("HOLA BUENAS");
+          System.out.println("MI NOMBRE ES MIGUEL");
+    }
+    
+}
